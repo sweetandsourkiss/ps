0c502638bacfa130592bde26b0d23989885a54b1
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-01-04 19:49:31 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+const [A, B] = input[0].split(" ").map(v => parseInt(v))
+
+const answer = A > B ? '>' : A < B ? '<' : '=='
+
+console.log(answer)
