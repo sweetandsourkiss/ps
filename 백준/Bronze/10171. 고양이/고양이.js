71b2e0894163f1049ecae6a08088cca289be3dd8
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-01-25 10:43:08 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 9268 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,7 @@
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const answer = "\\    /\\\n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|";
+console.log(answer);
