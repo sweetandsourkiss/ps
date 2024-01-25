534a73a3a730bd5e9e7c384dbd057a23ae221c43
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-01-25 10:49:22 +0900

    [Bronze V] Title: 개, Time: 104 ms, Memory: 9272 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,12 @@
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const answer =
+  "|\\_/|\n" +
+  "|q p|   /}\n" +
+  '( 0 )"""\\\n' +
+  '|"^"`    |\n' +
+  "||_/=\\\\__|\n";
+console.log(answer);
