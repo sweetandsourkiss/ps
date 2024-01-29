0015ca8cbd5e555b8c8a2c2be5c1789bec4ed95f
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-01-29 11:16:44 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 9280 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,13 @@
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+console.log(
+  "         ,r'\"7\n" +
+    "r`-_   ,'  ,/\n" +
+    " \\. \". L_r'\n" +
+    "   `~\\/\n" +
+    "      |\n" +
+    "      |",
+);
