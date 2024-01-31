f171cf2f4c3e127170fefa7866d2d7741f3bc57f
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-01-31 15:47:27 +0900

    [Silver V] Title: 단어 정렬, Time: 232 ms, Memory: 18924 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -4,12 +4,13 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 // your own code here
 let words = input.slice(1).filter((v) => v !== "");
-words = words.filter((v, index) => words.indexOf(v) === index); // 중복 제거
+// words = words.filter((v, index) => words.indexOf(v) === index); // 중복 제거
 words.sort((a, b) => {
   if (a.length === b.length) {
     return a < b ? -1 : a > b ? 1 : 0;
   }
   return a.length - b.length;
 });
+words = new Set(words);
 
-console.log(words.join("\n"));
+console.log(Array.from(words).join("\n"));
