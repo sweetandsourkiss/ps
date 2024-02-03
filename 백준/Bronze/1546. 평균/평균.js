2239be33d52163abf478486b37c973e60bb20d39
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-03 22:11:48 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+let numbers = input[1].split(" ").map((v) => parseInt(v));
+const max = numbers.reduce((prev, cur) => {
+  if (prev < cur) return cur;
+  else return prev;
+}, 0);
+numbers = numbers.map((v) => (v / max) * 100);
+const answer = numbers.reduce((prev, cur) => prev + cur, 0) / numbers.length;
+
+console.log(answer);
