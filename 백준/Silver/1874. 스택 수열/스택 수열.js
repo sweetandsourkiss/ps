23b770f8f392223aa3d5f1bb5d44a82496f7e653
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-06 23:32:53 +0900

    [Silver II] Title: 스택 수열, Time: 260 ms, Memory: 32964 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -4,7 +4,6 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 // your own code here
 const number = parseInt(input[0]);
-const oneToNumber = Array.from({ length: number }, (__, index) => index + 1);
 const printSequence = input
   .slice(1)
   .filter((v) => v !== "")
@@ -14,16 +13,15 @@ const nowArray = [];
 let numberPushedUntil = 0;
 let answer = "";
 
-while (printSequence.length > 0) {
-  const target = printSequence.shift();
+for (let nowIndex = 0; nowIndex < number; nowIndex++) {
+  const target = printSequence[nowIndex];
   if (target > numberPushedUntil) {
-    for (var blank of Array(target - numberPushedUntil)) {
-      nowArray.push(oneToNumber.shift());
+    while (numberPushedUntil < target) {
+      nowArray.push(++numberPushedUntil);
       answer += "+\n";
     }
     nowArray.pop();
     answer += "-\n";
-    numberPushedUntil = target;
   } else {
     if (nowArray[nowArray.length - 1] === target) {
       nowArray.pop();
