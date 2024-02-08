fc609cc6352c47adfdcd5dcccc301e1f0aab2dd3
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-08 11:18:58 +0900

    [Silver III] Title: 소수 구하기, Time: 320 ms, Memory: 24676 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const [from, to] = input[0].split(" ").map((v) => parseInt(v));
+const positiveIntegers = Array.from({ length: to + 1 }, () => true);
+
+for (let i = 2; i * 2 <= to; i++) {
+  if (positiveIntegers[i]) {
+    for (let j = i * 2; j <= to; j += i) {
+      positiveIntegers[j] = false;
+    }
+  }
+}
+
+let answer = "";
+
+positiveIntegers[0] = false;
+positiveIntegers[1] = false;
+for (let i = 2; i < from; i++) {
+  positiveIntegers[i] = false;
+}
+positiveIntegers.forEach((value, index) => {
+  if (value) {
+    answer += `${index}\n`;
+  }
+});
+
+console.log(answer);
