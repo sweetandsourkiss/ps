81299be439299ead8c5482aeda55e22ee6322ca7
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-14 22:04:15 +0900

    [Bronze II] Title: 벌집, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze II/2292. 벌집/벌집.js
+++ 백준/Bronze II/2292. 벌집/벌집.js
@@ -4,19 +4,12 @@ const input = fs.readFileSync(filePath).toString().split("\n");
 
 // your own code here
 const targetRoom = parseInt(input[0]);
-let interval = 6;
-let frontTerminal = 1;
+let maxValueInRange = 1;
+let range = 1;
 
-if (targetRoom === 1) {
-  console.log(1);
-} else {
-  for (let range = 2; ; range++) {
-    if (targetRoom > frontTerminal && targetRoom <= frontTerminal + interval) {
-      console.log(range);
-      return;
-    } else {
-      frontTerminal += interval;
-      interval += 6;
-    }
-  }
+while (targetRoom > maxValueInRange) {
+  maxValueInRange += 6 * range;
+  range++;
 }
+
+console.log(range);
