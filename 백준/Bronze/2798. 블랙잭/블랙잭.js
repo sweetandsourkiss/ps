1419d1e4f6df13df5bfb655a7bc9ed1b20dc8b10
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-19 12:43:18 +0900

    [Bronze II] Title: 블랙잭, Time: 212 ms, Memory: 9664 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const [numberOfCards, jackNumber] = input[0].split(" ").map((v) => parseInt(v));
+const cards = input[1].split(" ").map((v) => parseInt(v));
+
+let answer = 0;
+
+for (
+  let firstCardIndex = 0;
+  firstCardIndex < numberOfCards - 2;
+  firstCardIndex++
+) {
+  for (
+    let secondCardIndex = firstCardIndex + 1;
+    secondCardIndex < numberOfCards - 1;
+    secondCardIndex++
+  ) {
+    for (
+      let thirdCardIndex = secondCardIndex + 1;
+      thirdCardIndex < numberOfCards;
+      thirdCardIndex++
+    ) {
+      if (answer === jackNumber) {
+        console.log(jackNumber);
+        return;
+      }
+      const tmpSum =
+        cards[firstCardIndex] + cards[secondCardIndex] + cards[thirdCardIndex];
+      if (tmpSum <= jackNumber) {
+        answer = Math.max(answer, tmpSum);
+      }
+    }
+  }
+}
+console.log(answer);
