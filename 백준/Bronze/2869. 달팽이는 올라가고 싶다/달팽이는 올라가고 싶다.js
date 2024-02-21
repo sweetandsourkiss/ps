71734d1c0cde8541aa6e9e3682123d1183724b16
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-21 13:04:51 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const [upHeight, downHeight, stickHeight] = input[0]
+  .split(" ")
+  .map((v) => parseInt(v));
+const dailyHeight = upHeight - downHeight;
+
+let nowHeight = 0;
+
+const targetHeight = stickHeight - upHeight;
+
+if (targetHeight % dailyHeight === 0) {
+  console.log(targetHeight / dailyHeight + 1);
+} else {
+  console.log(Math.ceil(targetHeight / dailyHeight) + 1);
+}
