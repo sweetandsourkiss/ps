dd4de621997d72c3d45133f74e94de112fc38013
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-02-29 16:16:56 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 13048 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const commandNumber = Number(input[0]);
+const array = [];
+const answer = [];
+
+for (let c = 0; c < commandNumber; c++) {
+  const [text, number] = input[c + 1].split(" ");
+  if (text === "push") {
+    array.push(Number(number));
+  } else if (text === "top") {
+    if (array.length === 0) {
+      answer.push(-1);
+    } else {
+      answer.push(array[array.length - 1]);
+    }
+  } else if (text === "size") {
+    answer.push(array.length);
+  } else if (text === "empty") {
+    if (array.length === 0) {
+      answer.push(1);
+    } else {
+      answer.push(0);
+    }
+  } else if (text === "pop") {
+    if (array.length === 0) {
+      answer.push(-1);
+    } else {
+      answer.push(array.pop());
+    }
+  }
+}
+
+console.log(answer.join("\n"));
