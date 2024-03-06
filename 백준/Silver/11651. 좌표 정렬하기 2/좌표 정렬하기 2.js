4a790ede42227307267b20f61f2803e43c8fb2da
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-03-06 09:59:21 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 464 ms, Memory: 57592 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// your own code here
+const numberOfCoordinate = Number(input[0]);
+let coordinates = input
+  .slice(1, 1 + numberOfCoordinate)
+  .map((v) => v?.split(" ").map(Number));
+coordinates = coordinates
+  .sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]))
+  .map((v) => v.join(" "));
+console.log(coordinates.join("\n"));
