93593e934192d0b378c38fc6996c582d7b0206e1
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2024-04-14 23:49:57 +0900

    [Silver I] Title: 경로 찾기, Time: 200 ms, Memory: 10256 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.js
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.js
@@ -5,42 +5,21 @@ const input = fs.readFileSync(file_path).toString().split("\n");
 const size = +input[0];
 const adj_array = Array.from({ length: size }, () => Array(size));
 let index = 1;
+
 for (let r = 0; r < size; r++) {
   adj_array[r] = input[index++].split(" ").map(Number);
 }
 
-let answer = "";
-const find_way = (start, end) => {
-  const queue = [start];
-  const visited = Array(size).fill(false);
-  let flag = false;
-  while (queue.length > 0) {
-    const now = queue.shift();
+for (let k = 0; k < size; k++) {
+  for (let r = 0; r < size; r++) {
     for (let c = 0; c < size; c++) {
-      if (adj_array[now][c] === 1 && !visited[c]) {
-        if (c === end) {
-          flag = true;
-          break;
-        } else {
-          visited[c] = true;
-          queue.push(c);
-        }
+      if (adj_array[r][k] === 1 && adj_array[k][c] === 1) {
+        adj_array[r][c] = 1;
       }
     }
   }
-
-  if (flag) {
-    answer += "1 ";
-  } else {
-    answer += "0 ";
-  }
-};
-
-for (let r = 0; r < size; r++) {
-  for (let c = 0; c < size; c++) {
-    find_way(r, c);
-  }
-  answer += "\n";
 }
 
-console.log(answer);
+for (let r = 0; r < size; r++) {
+  console.log(adj_array[r].join(" "));
+}
