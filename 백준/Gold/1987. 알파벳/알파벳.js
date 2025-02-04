0170c9e0616695b3dfe54d4e06c7da31f4e2ffc8
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2025-02-04 23:26:15 +0900

    [Gold IV] Title: 알파벳, Time: 2532 ms, Memory: 15176 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
 
 /**
  * @author sweetandsourkiss
@@ -7,44 +8,39 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
  * @description 2025.02.04
  */
 
-const [R, C] = input[0].split(' ').map(Number);
-const board = input.slice(1).map(row => row.trim().split(''));
+const [R, C] = input[0].split(" ").map(Number);
 
-const delta = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-let maxResult = 0;
+const board = [];
 
-// 유니크 문자 수 계산
-const uniqueChars = new Set(board.flat());
-const totalUnique = uniqueChars.size;
-
-function isValid(x, y) {
-    return x >= 0 && x < R && y >= 0 && y < C;
+const CHAR_CODE_OF_A = 65;
+for (let i = 1; i <= R; i++) {
+  const chars = input[i].trim().split("");
+  board.push(chars);
 }
 
-function countBits(n) {
-    n = n - ((n >> 1) & 0x55555555);
-    n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
-    return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
-}
+const delta = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
 
-function dfs(x, y, mask) {
-    const charCode = board[x][y].charCodeAt(0) - 65;
-    const newMask = mask | (1 << charCode);
-    
-    maxResult = Math.max(maxResult, countBits(newMask));
-    
-    if (maxResult === totalUnique) return;
-    
-    for (const [dx, dy] of delta) {
-        const nx = x + dx, ny = y + dy;
-        if (isValid(nx, ny)) {
-            const nextCharCode = board[nx][ny].charCodeAt(0) - 65;
-            if (!(newMask & (1 << nextCharCode))) {
-                dfs(nx, ny, newMask);
-            }
-        }
+let result = 0;
+
+const dfs = (nowR, nowC, visited, cnt) => {
+  delta.forEach(([r, c]) => {
+    const newR = nowR + r;
+    const newC = nowC + c;
+    if (newR >= 0 && newR < R && newC >= 0 && newC < C) {
+      const charBit = 1 << (board[newR][newC].charCodeAt(0) - CHAR_CODE_OF_A);
+      if ((visited & charBit) === 0) {
+        dfs(newR, newC, visited | charBit, cnt + 1);
+      }
     }
-}
+  });
+  result = Math.max(result, cnt);
+};
 
-dfs(0, 0, 0);
-console.log(maxResult);
+dfs(0, 0, 1 << (board[0][0].charCodeAt(0) - CHAR_CODE_OF_A), 1);
+
+console.log(result);
