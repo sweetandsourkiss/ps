bce3283e2da053857701298ba65b3139ef4c5dd2
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2025-03-19 16:22:26 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,18 @@
+function solution(ineq, eq, n, m) {
+    if(ineq === '<' && eq === '='){
+        return n <= m ? 1 : 0;
+    }
+    
+    if(ineq === '<' && eq === '!'){
+        return n < m ? 1 : 0;
+    }
+    
+    if(ineq === '>' && eq === '='){
+        return n >= m ? 1 : 0;
+    }
+    
+    if(ineq === '>' && eq === '!'){
+        return n > m ? 1 : 0;
+    }
+    
+}
