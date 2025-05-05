f0e1ca005b4fd689d76facf0cbc97397260f66f7
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2025-05-05 16:29:46 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,4 +1,4 @@
+import math
+
 def solution(box, n):
-    for i in range (0, len(box)):
-        box[i] = int(box[i] / n)
-    return box[0] * box[1] * box[2]
+    return math.prod(map(lambda x : x // n, box))
