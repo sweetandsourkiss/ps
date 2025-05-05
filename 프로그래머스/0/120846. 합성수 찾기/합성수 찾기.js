1689aeb48cd9136e79f9885e7a7e2d0e9512128d
Jay Son <106135040+sweetandsourkiss@users.noreply.github.com>
2025-05-05 16:02:11 +0900

    [level 0] Title: 합성수 찾기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const arr = Array.from({length:n + 1}, ()=> false)
+    for(let i = 2; i <= parseInt(Math.sqrt(100)); i++){
+        if(!arr[i]) {
+            for(let j = i * 2; j <= n; j += i){
+                arr[j] = true
+            }
+        }
+    }
+    return arr.filter(v => v).length
+}
